cd57b9dffb9b91642ba8971b3f0753f2e2b82fe6
CarlsonLazorus <34118180+CarlsonLazorus@users.noreply.github.com>
2021-09-09 15:17:26 +0800

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+  <h5 class="q-mt-none">Help</h5>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit et tempora impedit, nulla itaque autem voluptate veniam! Cupiditate, harum laborum? Corrupti ipsa ea voluptate ipsam tenetur corporis molestias debitis laudantium?</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum veritatis voluptatibus explicabo cum eius ipsa? Temporibus numquam harum deleniti quo praesentium exercitationem repellendus, necessitatibus placeat esse, est delectus officia quos.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+ 
+})
+</script>
